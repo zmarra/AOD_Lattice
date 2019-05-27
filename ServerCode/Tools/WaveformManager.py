d8d89c7738b0cb115dc2fadd5e61df87c4cc2e22
brandongrinkemeyer <brandongrinkemeyer@gmail.com>
2019-05-27 13:44:58 -0500

    fixing phase randomizing methods

--- ServerCode/Tools/WaveformManager.py
+++ ServerCode/Tools/WaveformManager.py
@@ -14,11 +14,13 @@ class WaveformManager(object):
         self.waveformFile = waveformFile
         self.jsonData = self.getJsonData()
         self.initializeWaveforms()
+        self.modTime = os.stat(self.waveformFile).st_mtime
 
     def changeFile(self, file):
         self.waveformFile = file
         self.jsonData = self.getJsonData()
         self.initializeWaveforms()
+        self.modTime = os.stat(self.waveformFile).st_mtime
 
     def getJsonData(self):
         with open(self.waveformFile) as read_file:
@@ -28,8 +30,29 @@ class WaveformManager(object):
 
     def updateJsonData(self, jsonData):
         with open(self.waveformFile, 'w') as outfile:
-            json.dump(jsonData, self.waveformFile, indent=4, separators=(',', ': '))
+            json.dump(jsonData, outfile, indent=4, separators=(',', ': '))
         outfile.close()
+        self.jsonData = self.getJsonData()
+
+    def initializeWaveforms(self):
+        dataSize = int(np.floor(self.jsonData["Rate"] / self.jsonData['waveFreq']))
+        self.allWaves = []
+        for channel in self.jsonData["Channels"]:
+            self.allWaves.append([])
+            for currentWave in self.jsonData['Waves'][channel]:
+                wave = np.array(list(map(lambda n: waveforms['sine'](n, currentWave['freq'], self.jsonData["Rate"]), np.arange(dataSize, dtype=np.complex64))), dtype=np.complex64)
+                (self.allWaves[channel]).append(wave)
+
+    def randomizePhases(self, channel):
+        lines = len(self.jsonData['Waves'][channel])
+        ampThreshold = lines/1.5
+        maxAmp = ampThreshold + 1
+        while(maxAmp*2 > ampThreshold):
+            newPhases = [random.random() for _ in range(lines)]
+            self.changePhases(channel, newPhases)
+            maxAmp = self.getMaxAmp(channel)
+            print maxAmp
+        self.updateJsonData(self.jsonData)
 
     def getMaxAmp(self, channel):
         wave = self.getWaveform(channel)
@@ -49,68 +72,37 @@ class WaveformManager(object):
             i += 1
         return True
 
-    def randomizePhases(self, channel):
-        jsonData = self.getJsonData()
-        lines = len(jsonData['Waves'][channel])
-        ampThreshold = lines/1.5
-        maxAmp = ampThreshold + 1
-        while(maxAmp*2 > ampThreshold):
-            newPhases = [random.random() for _ in range(lines)]
-            self.changePhases(channel, newPhases)
-            maxAmp = self.getMaxAmp(channel)
-            print maxAmp
+    def getUpdateStatus(self):
+        oldModTime = self.modTime
+        self.modTime = os.stat(self.waveformFile).st_mtime
+        return self.modTime != oldModTime
+
+    def getWaveform(self, channel):
+        wave = self.allWaves[0][0]*0
+        i = 0
+        for currentWave in self.jsonData['Waves'][channel]:
+                wave = np.add(wave, currentWave['amplitude']*self.allWaves[channel][i]*np.exp(currentWave['phase']*np.pi*2j))
+                i += 1
+        return wave
+
+    def generateOutputWaveform(self):
+        outputWave = np.stack((self.getWaveform(0), self.getWaveform(1)), axis=0)
+        return outputWave
 
     def makeWaveform(self, freqsList, templateFile):
         data = {}
         with open(templateFile) as read_file:
             data = json.load(read_file)
-        for channel in len(freqsList):
+        print len(freqsList)
+        for channel in range(len(freqsList)):
             for freq in freqsList[channel]:
                 data['Waves'][channel].append({"freq": freq, "amplitude": .5, "phase": 0})
         self.updateJsonData(data)
-        self.randomizePhases()
+        self.randomizePhases(0)
+        self.randomizePhases(1)
 
-    def makeLatticeWaveform(self, lines, spacing, amplitudes, templateFile):
+    def makeLatticeWaveform(self, lines, spacing, templateFile):
         freqs = np.arange(-(lines-1)/2.0*spacing, ((lines-1)/2.0+1)*spacing, spacing)
         freqsList = [freqs, freqs]
-        self.makeWaveform(self, freqsList)
-
-    def getUpdateStatus(self):
-        oldModTime = self.modTimeOrig
-        modTime = os.stat(self.waveformFile).st_mtime
-        self.modTimeOrig = modTime
-        return modTime != oldModTime
-
-    def getWaveform(self, channel):
-        jsonData = self.getJsonData()
-        dataSize = int(np.floor(jsonData['Rate'] / jsonData['waveFreq']))
-        wave = np.array(list(map(lambda n: 0, np.arange(dataSize, dtype=np.complex64))), dtype=np.complex64)
-        for waveParameters in jsonData['Waves'][channel]:
-            currentWave = np.array(list(map(lambda n: waveParameters['amplitude'] * waveforms['sine'](n, waveParameters['freq'], jsonData['Rate']), np.arange(dataSize, dtype=np.complex64))), dtype=np.complex64)
-            wave = np.add(wave, np.exp(waveParameters['phase']*np.pi*2j) * currentWave)
-        return wave
-
-    def initializeWaveforms(self):
-        self.allWaves = []
-        for channel in self.jsonData["Channels"]:
-            (self.allWaves).append(self.generateWaveforms(channel))
-
-    def generateWaveforms(self, channel):
-        dataSize = int(np.floor(self.jsonData["Rate"] / self.jsonData['waveFreq']))
-        outputWaveform = []
-        for currentWave in self.jsonData['Waves'][channel]:
-            wave = np.array(list(map(lambda n: waveforms['sine'](n, currentWave['freq'], self.jsonData["Rate"]), np.arange(dataSize, dtype=np.complex64))), dtype=np.complex64)
-            outputWaveform.append(wave)
-        return outputWaveform
-
-    def generateOutputWaveform(self, jsonData):
-        waves = []
-        for channel in range(len(jsonData['Channels'])):
-            wave = self.allWaves[0][0]*0
-            i = 0
-            for currentWave in jsonData['Waves'][channel]:
-                    wave = np.add(wave, currentWave['amplitude']*self.allWaves[channel][i]*np.exp(currentWave['phase']*np.pi*2j))
-                    i += 1
-            waves.append(wave)
-        outputWave = np.stack((waves[0], waves[1]), axis=0)
-        return outputWave
+        print freqsList
+        self.makeWaveform(freqsList, templateFile)
